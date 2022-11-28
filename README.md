6954201650b1d92394279e8ae1678012665c51c9
Per Grapatin <per.grapatin@gmail.com>
2022-11-28 21:20:27 +0100

    template code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AOC2022
+This is my repo for the parts of AOC2022 that I have done
---
+++ dayTemplate/day.py
@@ -0,0 +1,34 @@
+"""
+Template code
+"""#import cmath for complex number operations
+#from abc import abstractproperty
+#import cmath
+#import Path for file operations
+from pathlib import Path
+
+PROGBLEM_INPUT_TXT = Path("/Users/pergrapatin/Source/AOC2021/src/"\
+    +"dayXX/input.txt").read_text()
+
+EXAMPLE_INPUT1 = """"""
+EXAMPLE_RESULT1 = 1514
+
+def string_worker(input_string):
+    """Helper string worker function
+    """
+    a_steps = input_string.split("\n")
+    a_steps = [int(number) for number in input_string.split(',')]
+    return a_steps
+
+def problem_a(input_string, expected_result):
+    """Problem A solved function
+    """
+    solution = input_string
+
+    if solution == expected_result:
+        print("Correct solution found:", solution)
+    else:
+        print("Incorrect solution, we got:", solution, "expected:", expected_result)
+
+problem_a(EXAMPLE_INPUT1, EXAMPLE_RESULT1)
+problem_a(PROGBLEM_INPUT_TXT, 0)
+print("\n")
