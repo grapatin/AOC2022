60f02c174f73811d52e0e2439df39af76698420c
Per Grapatin <per.grapatin@gmail.com>
2022-12-06 15:21:45 +0100

    day6 solved

--- day6/day.py
+++ day6/day.py
@@ -9,8 +9,8 @@ from pathlib import Path
 PROGBLEM_INPUT_TXT = Path("/Users/pergrapatin/Source/AOC2022/"\
     +"day6/input.txt").read_text()
 
-EXAMPLE_INPUT1 = """"""
-EXAMPLE_RESULT1 = 1514
+EXAMPLE_INPUT1 = """nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"""
+EXAMPLE_RESULT1 = 29
 
 def string_worker(input_string):
     """Helper string worker function
@@ -19,26 +19,25 @@ def string_worker(input_string):
     #a_steps = [int(number) for number in input_string.split(',')]
     return a_steps
 
-def problem_a(input_string, expected_result):
-    """Problem A solved function
-    """
-    solution = 0
-
-    rows = string_worker(input_string)
-
-    if solution == expected_result:
-        print("Correct solution found:", solution)
-    else:
-        print("Incorrect solution, we got:", solution, "expected:", expected_result)
-
-problem_a(EXAMPLE_INPUT1, EXAMPLE_RESULT1)
-problem_a(PROGBLEM_INPUT_TXT, 0)
-print("\n")
-
 def problem_b(input_string, expected_result):
     """Problem A solved function
     """
     solution = 0
+
+    for l in range(len(input_string)):
+        storageD = {}
+        found = True
+        for k in range(14):
+            if input_string[l + k] in storageD:
+                found = False
+                break
+            else:
+                storageD[input_string[l + k]] = True
+
+        if found:
+            solution = l + 14
+            break
+
 
     if solution == expected_result:
         print("Correct solution found:", solution)
@@ -46,5 +45,6 @@ def problem_b(input_string, expected_result):
         print("Incorrect solution, we got:", solution, "expected:", expected_result)
 
 problem_b(EXAMPLE_INPUT1, EXAMPLE_RESULT1)
-problem_b(PROGBLEM_INPUT_TXT, 0)
+problem_b(PROGBLEM_INPUT_TXT, 2635)
 print("\n")
+
